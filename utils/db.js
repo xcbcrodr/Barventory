da1a003854b11d04af141ca027039860f59d64dd
Cristian R Rodriguez P <34278180+xcbcrodr@users.noreply.github.com>
2025-07-18 17:58:25 -0500

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -1,21 +1,16 @@
 const { Pool } = require('pg');
-//require('dotenv').config();
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: Number(process.env.DB_PORT), 
+    connectionString: process.env.DATABASE_URL,
     ssl: {
-        rejectUnauthorized: false
+        rejectUnauthorized: false,
     },
     connectionTimeoutMillis: 10000,
     idleTimeoutMillis: 30000,
     max: 20
 });
 
-pool.on('error', (err, client) => {
+pool.on('error', (err) => {
     console.error('Error inesperado en el pool de clientes de PostgreSQL', err);
 });
 
